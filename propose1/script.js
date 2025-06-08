a5d0ef6261310ba4bd8bf966382476d14bf579ce
utsab <utsabsarkar00@gmail.com>
2025-06-08 22:52:50 +0530

    1

--- propose1/script.js
+++ propose1/script.js
@@ -1,12 +1,20 @@
-document.querySelector("#play").addEventListener("click", () => {
+// setTimeout(() => {
+//   document.querySelector("#btnDivContainer").style.display = "grid";
+// }, 1000);
+
+const playBtn = document.querySelector("#play");
+
+playBtn.addEventListener("click", () => {
+  playBtn.closest("#btnDivContainer").style.display = "none";
   const video = document.querySelector("#video");
   const song = document.querySelector("#song");
+
   video.play();
   song.play();
-  
+
   setTimeout(() => {
-    document.querySelector(".mainContainer").style.backgroundImage =
-      "url('heartsGif.gif')";
+    document.querySelector(".graphicContainer>.inner").style.backgroundImage =
+      "url('heart2.gif')";
   }, 4000);
 
   setTimeout(() => {
