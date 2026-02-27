8d6bf1847ef3277128a7a0278a3eb9d58608efed
utsab00 <utsab7915@sigilotech.com>
2026-02-28 01:54:27 +0530

    propose-common

---
+++ propose1/script.js
@@ -0,0 +1,42 @@
+const startBtn = document.querySelector("#startBtn");
+const introPage = document.querySelector("#introPage");
+const proposalSection = document.querySelector("#proposalSection");
+
+const video = document.querySelector("#video");
+const song = document.querySelector("#song");
+const finalQuestion = document.querySelector("#finalQuestion");
+
+
+startBtn.addEventListener("click", () => {
+
+  introPage.style.opacity = "0";
+
+  setTimeout(() => {
+    introPage.style.display = "none";
+    proposalSection.style.display = "block";
+
+    video.play();
+    song.play();
+
+    // Add heart background (NO REPEAT)
+    setTimeout(() => {
+      const inner = document.querySelector(".graphicContainer > .inner");
+      inner.style.backgroundImage = "url('heart2.gif')";
+      inner.style.backgroundRepeat = "no-repeat";
+      inner.style.backgroundPosition = "center";
+      inner.style.backgroundSize = "contain";
+    }, 4000);
+
+    // Show floating heart gif
+    setTimeout(() => {
+      document.querySelector("#heartDiv").style.display = "block";
+      document.querySelector("#heartDiv").classList.add("show");
+    }, 5000);
+
+    // Show final question
+    setTimeout(() => {
+      finalQuestion.classList.add("show");
+    }, 8000);
+
+  }, 600);
+});
